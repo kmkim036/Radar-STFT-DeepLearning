f5af6aa9849cec15fa6ca7f2c66def36807902c7
Kim Kyeong-min <kmkim036@gmail.com>
2021-11-09 13:31:13 +0900

    Update: latest version

--- src/reference/SoC_LAB/ComLib_Matlab_Interface/RadarSystemExamples/GettingStarted/extract_raw_data.m
+++ src/reference/SoC_LAB/ComLib_Matlab_Interface/RadarSystemExamples/GettingStarted/extract_raw_data.m
@@ -29,9 +29,12 @@
 % DESCRIPTION:
 % This simple example demos the acquisition of data.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+% should execute this file at 'Radar-CWT-DeepLearning' dircetory
+
 %% cleanup and init
 % Before starting any kind of device the workspace must be cleared and the
-% Matlab Interface must be included into the code.
+% Matlab Interface must be included into the code. 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc
 disp('******************************************************************');
@@ -41,7 +44,7 @@ close all
 resetRS; % close and delete ports
 
 % Configuration: Change to you needs
-serialPortName = 'COM6'; % Input the serial port name which is shown in device manager
+serialPortName = 'COM6';   % Input the serial port name which is shown in device manager
 
 % Constants: Do not change unless this value is also changed in FW
 sampling_freq_hz = 3000;
@@ -51,49 +54,48 @@ serialPortHandle = radar_open_device(serialPortName);
 disp('Connected RadarSystem:');
 
 time_cnt = 1;
-TIME = 150; % �� ���� Ƚ��
+TIME = 20; % 총 추출 횟수
 % tic
 % Getting raw data
 while true
     % Trigger radar chirp and get the raw data of single chirp
-
-    [I, Q] = radar_get_frame(serialPortHandle);
-
-    % Raw Data ���� ����
-    if (time_cnt <= TIME)
-
+    
+    [I, Q] = radar_get_frame(serialPortHandle);   
+    
+    % Raw Data 추출 과정
+    if(time_cnt <= TIME)
         if time_cnt == 1
             dump_I = I;
             dump_Q = Q;
         else
             dump_I = [dump_I, I];
             dump_Q = [dump_Q, Q];
-        end
-
+        end        
     else
-        break; % �� ���� Ƚ���� ä��� while���� ��������
-    end
-
+        break; % 총 누적 횟수를 채우면 while문을 빠져나옴
+    end        
     time_cnt = time_cnt + 1;
-
-    %     ydata = complex(I,Q);
-    %     disp(ydata);
+    
+%     ydata = complex(I,Q);
+%     disp(ydata);
 end;
-
 % toc
 % Close connection to the device
 radar_close_device(serialPortHandle);
 
 %% File Out
-filename = 'TEST3'; % ���ϸ� ����!
+filename = '211109_1_2'; % 파일명 변경!
+foldername = 'data\211109\';
 
 dump_I_vector = reshape(dump_I, numel(dump_I), 1);
 dump_Q_vector = reshape(dump_Q, numel(dump_Q), 1);
 
-dlmwrite(['DATA\', filename, '_RE.txt'], dump_I_vector(:), 'delimiter', '', 'newline', 'pc');
-dlmwrite(['DATA\', filename, '_IM.txt'], dump_Q_vector(:), 'delimiter', '', 'newline', 'pc');
+dlmwrite([foldername,filename,'_RE.txt'], dump_I_vector(:), 'delimiter', '', 'newline', 'pc');
+dlmwrite([foldername,filename,'_IM.txt'], dump_Q_vector(:), 'delimiter', '', 'newline', 'pc');
 
-%%
+
+
+%% plot
 RawData = complex(dump_I, dump_Q);
 % DC removal
 RawData_DC = RawData - mean(RawData);
@@ -103,8 +105,31 @@ RawData_DC_vector = reshape(RawData_DC, numel(RawData_DC), 1);
 N_FFT = 128;
 Window = hamming(N_FFT);
 SamplingFreq = 3e+3;
-Overlap_Len = N_FFT / 2; % 50 % overlapping
+Overlap_Len = N_FFT/2; % 50% overlapping
 % STFT
 stft_data = stft(RawData_DC_vector, SamplingFreq, 'Window', Window, 'OverlapLength', Overlap_Len, 'FFTLength', N_FFT);
 % Plot
 figure; imagesc(pow2db(abs(stft_data))); colorbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
