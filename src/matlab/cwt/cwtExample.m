f072bae2f4feb4674ce13a565b4d8483c90b9f3d
Kim Kyeong-min <kmkim036@gmail.com>
2021-11-14 21:54:55 +0900

    Add: cwt for absolute value

--- src/matlab/cwt/cwtExample.m
+++ src/matlab/cwt/cwtExample.m
@@ -1,13 +1,16 @@
 %% by kmkim
 % need to install wavelet toolbox before
 
+%% close all ex-figures
+close all;
+
 %% index just for figure numbering
 i = 1;
 
 %% Load a raw radar signal data
 % revise file name to load
-load 211109_4_4_RE.mat;
-load 211109_4_4_IM.mat;
+load 211029_2_2_RE.mat;
+load 211029_2_2_IM.mat;
 
 %% signal processing and plot stft (by SoC Lab code)
 % make into complex raw data
@@ -25,10 +28,10 @@ Overlap_Len = N_FFT / 2; % 50 % overlapping
 stft_data = stft(RawData_DC_vector, SamplingFreq, 'Window', Window, 'OverlapLength', Overlap_Len, 'FFTLength', N_FFT);
 % Plot
 figure(i);
+i = i + 1;
 imagesc(pow2db(abs(stft_data)));
 colorbar;
 title('STFT result');
-i = i + 1;
 
 %% plot raw signal data by real and imagenary
 % figure(i);
@@ -53,88 +56,168 @@ i = i + 1;
 % To perform the Continuous Wavelet Transform, you can use the fucntion cwt. We provide the
 % signal and the sampling frequency as an input arguments.
 
-%% cwt with morse wavelet
+mode = 1; % variable 'mode' to select input data
+% 1: abs before transform / DC not removed
+% 2: abs before transform after DC removed
+
+if mode == 1
+    input = abs(RawData);
+    input_raw = RawData;
+elseif mode == 2
+    input = abs(RawData_DC_vector);
+    input_raw = RawData_DC_vector;
+end
+
+% morse wavelet
+cwt_data = cwt(input, Fs);
 figure(i);
 i = i + 1;
-cwt_data = cwt(dataRE, Fs); % default wavelet = morse wavelet
+subplot(2, 1, 1);
+imagesc(abs(cwt_data));
+colorbar;
+title('CWT with morse wavelet');
+subplot(2, 1, 2);
 imagesc(pow2db(abs(cwt_data)));
 colorbar;
-title('CWT with Morse wavelet Real Part');
+title('CWT with morse wavelet with log scale');
 
+% bump wavelet
+cwt_data = cwt(input, 'bump', Fs);
 figure(i);
 i = i + 1;
-cwt_data = cwt(dataIM, Fs); % default wavelet = morse wavelet
+subplot(2, 1, 1);
+imagesc(abs(cwt_data));
+colorbar;
+title('CWT with bump wavelet');
+subplot(2, 1, 2);
 imagesc(pow2db(abs(cwt_data)));
 colorbar;
-title('CWT with Morse wavelet Imaginary Part');
+title('CWT with bump wavelet with log scale');
 
-%% cwt with bump wavelet
+% morlet wavelet
+cwt_data = cwt(input, 'amor', Fs);
 figure(i);
 i = i + 1;
-cwt_data = cwt(dataRE, 'bump', Fs); % bump wavelet
+subplot(2, 1, 1);
+imagesc(abs(cwt_data));
+colorbar;
+title('CWT with morlet wavelet');
+subplot(2, 1, 2);
 imagesc(pow2db(abs(cwt_data)));
 colorbar;
-title('CWT with bump wavelet Real Part');
+title('CWT with morlet wavelet with log scale');
 
-figure(i);
-i = i + 1;
-cwt_data = cwt(dataIM, 'bump', Fs); % bump wavelet
-imagesc(pow2db(abs(cwt_data)));
-colorbar;
-title('CWT with bump wavelet Imaginary Part');
-
-%% cwt with analytic morlet wavelet
-figure(i);
-i = i + 1;
-cwt_data = cwt(dataRE, 'amor', Fs); % analytic Morlet wavelet
-imagesc(pow2db(abs(cwt_data)));
-colorbar;
-title('CWT with analytic Morlet wavelet Real Part');
-
-figure(i);
-i = i + 1;
-cwt_data = cwt(dataIM, 'amor', Fs); % analytic Morlet wavelet
-imagesc(pow2db(abs(cwt_data)));
-colorbar;
-title('CWT with analytic Morlet wavelet Imaginary Part');
-
-%% compare cwt data of real, imagenary with complex
-% Rawdata = complex(dataRE, dataIM);
+%% complex positive, negative
+% cwt_data_0 = cwt(input_raw, 'amor', Fs);
+% cwt_data_1 = cwt_data_0(:,:,1); 
+% cwt_data_2 = cwt_data_0(:,:,2);
+% cwt_data_3 = cwt_data_1 + cwt_data_2;
+% cwt_data_4 = abs(cwt_data_1) + abs(cwt_data_2);
+% 
 % figure(i);
-% cwt(dataRE, Fs);
-% title('real');
 % i = i + 1;
+% subplot(2, 1, 1);
+% imagesc(abs(cwt_data_1));
+% colorbar;
+% title('positive component with morlet wavelet');
+% subplot(2, 1, 2);
+% imagesc(pow2db(abs(cwt_data_1)));
+% colorbar;
+% title('positive component with morlet wavelet, log scale');
+% 
 % figure(i);
-% cwt(dataIM, Fs);
-% title('im');
-% i=i+1;
+% i = i + 1;
+% subplot(2, 1, 1);
+% imagesc(abs(cwt_data_2));
+% colorbar;
+% title('negative component with morlet wavelet');
+% subplot(2, 1, 2);
+% imagesc(pow2db(abs(cwt_data_2)));
+% colorbar;
+% title('negative component with morlet wavelet, log scale');
+% 
 % figure(i);
-% cwt(Rawdata,Fs);
-% i = i+1;
+% i = i + 1;
+% subplot(2, 1, 1);
+% imagesc(abs(cwt_data_3));
+% colorbar;
+% title('abs(pos + neg) with morlet wavelet');
+% subplot(2, 1, 2);
+% imagesc(pow2db(abs(cwt_data_3)));
+% colorbar;
+% title('abs(pos + neg) with morlet wavelet, log scale');
+% 
+% figure(i);
+% i = i + 1;
+% subplot(2, 1, 1);
+% imagesc(cwt_data_4);
+% colorbar;
+% title('abs(pos) + abs(neg) with morlet wavelet');
+% subplot(2, 1, 2);
+% imagesc(pow2db(cwt_data_4));
+% colorbar;
+% title('abs(pos) + abs(neg) with morlet wavelet, log scale');
 
-% Result
-% positive component = left half side of real part
-% negative component = right half side of imagenary part
-
-%% compare cwt data of DC removal and vectoring with normal one
+%% real, imaginary respectively
+% % cwt with morse wavelet
+% figure(i);
+% i = i + 1;
+% cwt_data = cwt(dataRE, Fs); % default wavelet = morse wavelet
+% imagesc(pow2db(abs(cwt_data)));
+% colorbar;
+% title('CWT with Morse wavelet Real Part');
+% 
+% figure(i);
+% i = i + 1;
+% cwt_data = cwt(dataIM, Fs); % default wavelet = morse wavelet
+% imagesc(pow2db(abs(cwt_data)));
+% colorbar;
+% title('CWT with Morse wavelet Imaginary Part');
+% 
+% % cwt with bump wavelet 
+% figure(i);
+% i = i + 1;
+% cwt_data = cwt(dataRE, 'bump', Fs); % bump wavelet
+% imagesc(pow2db(abs(cwt_data)));
+% colorbar;
+% title('CWT with bump wavelet Real Part');
+% 
+% figure(i);
+% i = i + 1;
+% cwt_data = cwt(dataIM, 'bump', Fs); % bump wavelet
+% imagesc(pow2db(abs(cwt_data)));
+% colorbar;
+% title('CWT with bump wavelet Imaginary Part');
+% 
+% % cwt with analytic morlet wavelet 
 % figure(i);
 % i = i + 1;
 % cwt_data = cwt(dataRE, 'amor', Fs); % analytic Morlet wavelet
 % imagesc(pow2db(abs(cwt_data)));
 % colorbar;
-% title('CWT with analytic Morlet wavelet RE');
-%
-% dataRE = dataRE - mean(dataRE); % DC removal
-% dataRE_DC_vector = reshape(dataRE, numel(dataRE), 1); % Vectoring
-%
+% title('CWT with analytic Morlet wavelet Real Part');
+% 
 % figure(i);
 % i = i + 1;
-% cwt_data = cwt(dataRE_DC_vector, 'amor', Fs); % analytic Morlet wavelet
+% cwt_data = cwt(dataIM, 'amor', Fs); % analytic Morlet wavelet
 % imagesc(pow2db(abs(cwt_data)));
 % colorbar;
-% title('CWT with analytic Morlet wavelet RE DC vector');
+% title('CWT with analytic Morlet wavelet Imaginary Part');
 
-% Result
-% no difference
+%% compare cwt data of real, imagenary with complex
+% figure(i);
+% i = i + 1;
+% subplot(2, 1, 1);
+% imagesc(abs(cwt(dataRE, 'amor', Fs)));
+% colorbar();
+% title('Real');
+% subplot(2, 1, 2);
+% imagesc(abs(cwt(dataIM, 'amor', Fs)));
+% colorbar();
+% title('Imaginary');
+% 
+% figure(i);
+% i = i + 1;
+% cwt(RawData,'amor', Fs);
 
 %% EOF
