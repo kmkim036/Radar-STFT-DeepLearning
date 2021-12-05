0672b21f74db2c500d15d578b2530c0245e48abd
kmkim036 <kmkim036@gmail.com>
2021-12-05 23:59:21 +0900

    Add: over fitting & augmentation

--- src/python/deep_learning.py
+++ src/python/deep_learning.py
@@ -20,19 +20,32 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.utils import np_utils
-
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 date = '211130'
-mode = 1
+mode = 0
 
 if mode == 0:
     img_row = 128
     img_col = 29
     file_name = '_stft.csv'
 elif mode == 1:
+    img_row = 81
+    img_col = 120
+    file_name = '_cwt_16.csv'
+elif mode == 2:
+    img_row = 81
+    img_col = 96
+    file_name = '_cwt_20.csv'
+elif mode == 3:
     img_row = 26
     img_col = 384
-    file_name = '_cwt_crop.csv'
+    file_name = '_cwt.csv'
+elif mode == 4:
+    img_row = 40
+    img_col = 41
+    file_name = '_cwt_16_40.csv'
 
 # Directory Path Setting
 # kkm
@@ -47,7 +60,8 @@ DirectoryPath = '/home/kmkim/Projects/git/kmkim036/Radar-CWT-DeepLearning/'
 # ksj
 # DirectoryPath = ""
 
-# 80% => train
+# 60% => train
+# 20% => validation
 # 20% => test
 
 
@@ -109,12 +123,15 @@ def preprocessing(classnum):
 
 def preprocessing_by_person(person):
     whole_rounds = 80
-    whole_counts1 = int(whole_rounds * 0.8)
+    whole_counts1 = int(whole_rounds * 0.6)
     image1 = np.zeros(shape=(whole_counts1, img_row, img_col))
     label1 = []
     whole_counts2 = int(whole_rounds * 0.2)
     image2 = np.zeros(shape=(whole_counts2, img_row, img_col))
     label2 = []
+    whole_counts3 = int(whole_rounds * 0.2)
+    image3 = np.zeros(shape=(whole_counts3, img_row, img_col))
+    label3 = []
     i = 0
     for motion in range(0, 4):
         if motion < 2:
@@ -123,7 +140,7 @@ def preprocessing_by_person(person):
             motion_rounds = 10
         cwt_data = pd.read_csv(
             DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
-        for rounds in range(0, int(motion_rounds * 0.8)):
+        for rounds in range(0, int(motion_rounds * 0.6)):
             df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
             df = np.reshape(df, (img_row, img_col))
             image1[i] = df
@@ -137,14 +154,28 @@ def preprocessing_by_person(person):
             motion_rounds = 10
         cwt_data = pd.read_csv(
             DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
-        for rounds in range(int(motion_rounds * 0.8), motion_rounds):
+        for rounds in range(int(motion_rounds * 0.6), int(motion_rounds * 0.8)):
             df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
             df = np.reshape(df, (img_row, img_col))
             image2[i] = df
             label2.append(motion)
             i = i + 1
+    i = 0
+    for motion in range(0, 4):
+        if motion < 2:
+            motion_rounds = 30
+        else:
+            motion_rounds = 10
+        cwt_data = pd.read_csv(
+            DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
+        for rounds in range(int(motion_rounds * 0.8), motion_rounds):
+            df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
+            df = np.reshape(df, (img_row, img_col))
+            image3[i] = df
+            label3.append(motion)
+            i = i + 1
 
-    return image1, label1, image2, label2
+    return image1, label1, image2, label2, image3, label3
 
 
 def preprocessing_by_motion(motion):
@@ -152,19 +183,22 @@ def preprocessing_by_motion(motion):
         whole_rounds = 30
     else:
         whole_rounds = 10
-    whole_counts1 = int(whole_rounds * 0.8 * 3)
-    image1 = np.zeros(shape=(whole_counts1, img_row, img_col))
+    whole_counts1 = int(whole_rounds * 0.6 * 3)
+    image1 = np.zeros(shape=(whole_counts1, img_row, img_col, 1))
     label1 = []
     whole_counts2 = int(whole_rounds * 0.2 * 3)
-    image2 = np.zeros(shape=(whole_counts2, img_row, img_col))
+    image2 = np.zeros(shape=(whole_counts2, img_row, img_col, 1))
     label2 = []
+    whole_counts3 = int(whole_rounds * 0.2 * 3)
+    image3 = np.zeros(shape=(whole_counts3, img_row, img_col, 1))
+    label3 = []
     i = 0
     for person in range(0, 3):
         cwt_data = pd.read_csv(
             DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
-        for rounds in range(0, int(whole_rounds * 0.8)):
+        for rounds in range(0, int(whole_rounds * 0.6)):
             df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
-            df = np.reshape(df, (img_row, img_col))
+            df = np.reshape(df, (img_row, img_col, 1))
             image1[i] = df
             label1.append(person)
             i = i + 1
@@ -172,71 +206,108 @@ def preprocessing_by_motion(motion):
     for person in range(0, 3):
         cwt_data = pd.read_csv(
             DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
-        for rounds in range(int(whole_rounds * 0.8), whole_rounds):
+        for rounds in range(int(whole_rounds * 0.6), int(whole_rounds * 0.8)):
             df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
-            df = np.reshape(df, (img_row, img_col))
+            df = np.reshape(df, (img_row, img_col, 1))
             image2[i] = df
             label2.append(person)
             i = i + 1
+    i = 0
+    for person in range(0, 3):
+        cwt_data = pd.read_csv(
+            DirectoryPath + date + "_" + str(person) + "_" + str(motion) + file_name)
+        for rounds in range(int(whole_rounds * 0.8), whole_rounds):
+            df = np.fromstring(cwt_data['pixels'][rounds], dtype=int, sep=' ')
+            df = np.reshape(df, (img_row, img_col, 1))
+            image3[i] = df
+            label3.append(person)
+            i = i + 1
 
-    return image1, label1, image2, label2
+    return image1, label1, image2, label2, image3, label3
 
 
 def create_CNNmodel(classnum):
     model = Sequential()
-    # model 1
-    model.add(Conv2D(64, kernel_size=(5, 5), strides=(1, 1),
-                     padding='same', activation='relu', input_shape=(img_row, img_col, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    model.add(Conv2D(32, kernel_size=(2, 2), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+
+    model.add(Conv2D(32, (3, 3), activation='relu',
+              input_shape=(img_row, img_col, 1)))
+    model.add(MaxPooling2D(2, 2))
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+
     model.add(Flatten())
-    model.add(Dense(500, activation='relu'))
-    model.add(Dropout(0.5))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(classnum, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
                   optimizer='adam', metrics=['accuracy'])
-    # model 2
-    #  model.add(Conv2D(32, (3, 3), activation='relu',
-    #           input_shape=(img_row, img_col, 1)))
-    # model.add(MaxPooling2D(2, 2))
-    # model.add(Dropout(0.2))
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(2, 2))
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(2, 2))
-    # model.add(Dropout(0.2))
-    # model.add(Flatten())
-    # model.add(Dense(64, activation='relu'))
-    # model.add(Dense(classnum, activation='softmax'))
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
 if __name__ == "__main__":
     # person classify by motion
-    """
+    # """
     classnum = 3
     model = create_CNNmodel(classnum)
-    for i in range(0, 4):
-        train_set, train_label, test_set, test_label = preprocessing_by_motion(
+    early_stopping = EarlyStopping(monitor='val_accuracy', patience=10)
+    for i in range(0, 1):
+        train_set, train_label, val_set, val_label, test_set, test_label = preprocessing_by_motion(
             i)
         print("motion index: " + str(i))
         print("train set")
         print("counts: " + str(train_set.shape[0]))
         print("labels: " + str(train_label))
+        print("val set")
+        print("counts: " + str(val_set.shape[0]))
+        print("labels: " + str(val_label))
         print("test set")
         print("counts: " + str(test_set.shape[0]))
         print("labels: " + str(test_label))
+
         train_set = np.array(train_set)
+        val_set = np.array(val_set)
         test_set = np.array(test_set)
+
+        maxval = train_set.max()
+        if maxval < val_set.max():
+            maxval = val_set.max()
+        if maxval < test_set.max():
+            maxval = test_set.max()
+
+        train_set = train_set.astype('float32')/maxval
+        val_set = val_set.astype('float32')/maxval
+        test = test_set.astype('float32')/maxval
+
         train_label = np_utils.to_categorical(train_label, classnum)
+        val_label = np_utils.to_categorical(val_label, classnum)
         test_label = np_utils.to_categorical(test_label, classnum)
 
-        hist = model.fit(train_set, train_label, epochs=80, verbose = 0)
+        gen = ImageDataGenerator(
+            width_shift_range=0.2
+        )
+        augment_ratio = 1.5   # 전체 데이터의 150%
+        augment_size = int(augment_ratio * train_set.shape[0])
+
+        randidx = np.random.randint(train_set.shape[0], size=augment_size)
+
+        x_augmented = train_set[randidx].copy()
+        y_augmented = train_label[randidx].copy()
+
+        x_augmented, y_augmented = gen.flow(
+            x_augmented, y_augmented,  batch_size=augment_size, shuffle=False).next()
+
+        train_set = np.concatenate((train_set, x_augmented))
+        train_label = np.concatenate((train_label, y_augmented))
+        s = np.arange(train_set.shape[0])
+        np.random.shuffle(s)
+        train_set = train_set[s]
+        train_label = train_label[s]
+
+        hist = model.fit(train_set, train_label, validation_data=(
+            val_set, val_label), epochs=50, verbose=1, callbacks=[early_stopping])
+        # hist = model.fit(train_set, train_label, validation_data=(val_set, val_label), epochs=50, verbose=1)
 
         # evaluate: evaluate
         print('Evaluate')
@@ -245,44 +316,62 @@ if __name__ == "__main__":
         print('Test accuracy:', score[1])
 
         # predict: test
-        pred = model.predict(test_set)
-        if i < 2:
-            cnts = 18
-        else:
-            cnts = 6
-        corrects = 0
-        notcorrects = 0
-        for j in range(0, cnts):
-            if np.argmax(test_label[j]) == np.argmax(pred[j]):
-                corrects = corrects + 1
-            else:
-                notcorrects = notcorrects + 1
-            print("No." + str(j + 1) + " data: " +
-                  str(np.argmax(test_label[j])), ", predict: " + str(np.argmax(pred[j])))
-        print("predict accuracy: " + str(corrects / cnts))
-    """
+        # pred = model.predict(test_set)
+        # if i < 2:
+        #     cnts = 18
+        # else:
+        #     cnts = 6
+        # corrects = 0
+        # notcorrects = 0
+        # for j in range(0, cnts):
+        #     if np.argmax(test_label[j]) == np.argmax(pred[j]):
+        #         corrects = corrects + 1
+        #     else:
+        #         notcorrects = notcorrects + 1
+        #     print("No." + str(j + 1) + " data: " +
+        #           str(np.argmax(test_label[j])), ", predict: " + str(np.argmax(pred[j])))
+        # print("predict accuracy: " + str(corrects / cnts))
+    # """
 
     # motion classify by person
     """
     classnum = 4
+    early_stopping = EarlyStopping(monitor='val_accuracy', patience=10)
     model = create_CNNmodel(classnum)
     for i in range(0, 3):
-        train_set, train_label, test_set, test_label = preprocessing_by_person(
+        train_set, train_label, val_set, val_label, test_set, test_label = preprocessing_by_person(
             i)
         print("person index: " + str(i))
         print("train set")
         print("counts: " + str(train_set.shape[0]))
         print("labels: " + str(train_label))
+        print("val set")
+        print("counts: " + str(val_set.shape[0]))
+        print("labels: " + str(val_label))
         print("test set")
         print("counts: " + str(test_set.shape[0]))
         print("labels: " + str(test_label))
+
         train_set = np.array(train_set)
+        val_set = np.array(val_set)
         test_set = np.array(test_set)
+
+        maxval = train_set.max()
+        if maxval < val_set.max():
+            maxval = val_set.max()
+        if maxval < test_set.max():
+            maxval = test_set.max()
+
+        train_set = train_set.astype('float32')/maxval
+        val_set = val_set.astype('float32')/maxval
+        test = test_set.astype('float32')/maxval
+        
         train_label = np_utils.to_categorical(train_label, classnum)
+        val_label = np_utils.to_categorical(val_label, classnum)
         test_label = np_utils.to_categorical(test_label, classnum)
 
-        hist = model.fit(train_set, train_label, epochs=80, verbose = 0)
-
+        hist = model.fit(train_set, train_label, validation_data=(val_set, val_label), epochs=50, verbose = 1, callbacks=[early_stopping])
+        
         # evaluate: evaluate
         print('Evaluate')
         score = model.evaluate(test_set, test_label)
