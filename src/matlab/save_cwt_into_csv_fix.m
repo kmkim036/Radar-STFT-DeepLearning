08598911e1bb03ebf01a74538cb01860906013d2
kmkim036 <kmkim036@gmail.com>
2021-12-06 20:55:39 +0900

    Add: stft parts

--- src/matlab/save_cwt_into_csv_fix.m
+++ src/matlab/save_cwt_into_csv_fix.m
@@ -40,24 +40,21 @@ for person = 0:2
                     ext_data(ii,jj)=cwt_data(ii,index*jj);
                 end
             end    
-            
-            % need to fix crop part
-            % "fix row_start and column_start value by motion"
-            % extract time: 0.6s => 1920 * (0.6 / 3) = 384
-            % extract frequency: 10000 / 384 = 26.04
-            % => image size: 26 x 384
+            reshape_crop = reshape(ext_data', 1, []);
 
-            % column_start = (start second / 3) * 1920
-            % example: start second = 1.5 => column_start = (1.5 / 3) * 1920 = 960
+
+%           stft
+            % N_FFT = 128;
+            % Window = hamming(N_FFT);
+            % SamplingFreq = 3e+3;
+            % Overlap_Len = N_FFT / 2; % 50 % overlapping
+            % stft_data = stft(RawData_DC_vector, SamplingFreq, 'Window', Window, 'OverlapLength', Overlap_Len, 'FFTLength', N_FFT);
+            % reshape_crop = reshape(abs(stft_data)', 1, []); % 1행으로 쫙 펼침  
             
-            % cwt_data_crop = abs(cwt_data(1:39,40:80));
-            %  cwt_data_crop = abs(red_data(1:40,80:159));
+
+            % cwt_data_crop = abs(red_data(1:40,80:159));
             % reshape_crop = reshape(cwt_data_crop', 1, []); % 1행으로 쫙 펼침
             
-            % reshape_crop = reshape(ext_data', 1, []);
-
-            red_data = ext_data(1:40,40:80);
-            reshape_crop = reshape(red_data', 1, []); % 1행으로 쫙 펼침  
             sum_cwt = [sum_cwt; reshape_crop]; % 다음 행에 추가
 
         end
