fc55a192938225600197b73f689ad8d3a3d5f33b
kmkim036 <kmkim036@gmail.com>
2021-12-06 20:53:33 +0900

    Fix: stft sample freq

--- src/matlab/plot_transform.m
+++ src/matlab/plot_transform.m
@@ -9,8 +9,8 @@ i = 1;
 
 %% Load a raw radar signal data
 % revise file name to load
-load 211130_0_0_9_RE.mat;
-load 211130_0_0_9_IM.mat;
+load 211130_0_0_1_RE.mat;
+load 211130_0_0_1_IM.mat;
 
 %% signal processing and plot stft (by SoC Lab code)
 % make into complex raw data
@@ -22,13 +22,14 @@ RawData_DC_vector = reshape(RawData_DC, numel(RawData_DC), 1);
 % Parameter Load
 N_FFT = 128;
 Window = hamming(N_FFT);
-SamplingFreq = 3e+3;
+SamplingFreq = 650;
 Overlap_Len = N_FFT / 2; % 50 % overlapping
 % STFT
 stft_data = stft(RawData_DC_vector, SamplingFreq, 'Window', Window, 'OverlapLength', Overlap_Len, 'FFTLength', N_FFT);
 % Plot
 figure(i);
 i = i + 1;
+stft_data = stft(RawData_DC_vector, SamplingFreq, 'Window', Window, 'OverlapLength', Overlap_Len, 'FFTLength', N_FFT);
 imagesc(pow2db(abs(stft_data)));
 colorbar;
 title('STFT result');
