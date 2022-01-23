694f0be1196b9cb1dbf58f6484d6088f46e9dbe3
kmkim036 <kmkim036@gmail.com>
2022-01-23 23:15:02 +0900

    Add: other models

--- src/python/deep_learning_model.py
+++ src/python/deep_learning_model.py
@@ -8,7 +8,7 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 def create_CNNmodel(img_row, img_col, classnum):
     model = Sequential()
 
-    # model 0
+    # model 1
     # model.add(Conv2D(32, (3, 3), activation='relu',
     #           input_shape=(img_row, img_col, 1)))
     # model.add(MaxPooling2D(2, 2))
@@ -19,23 +19,6 @@ def create_CNNmodel(img_row, img_col, classnum):
 
     # model.add(Flatten())
     # model.add(Dense(64, activation='relu'))
-    # model.add(Dense(classnum, activation='softmax'))
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer='adam', metrics=['accuracy'])
-
-    # model 1
-    # model = Sequential()
-    # model.add(Conv2D(12, kernel_size=(5, 5), activation='relu', input_shape=(img_row, img_col, 1)))
-    # model.add(MaxPooling2D(2, 2))
-
-    # model.add(Conv2D(16, kernel_size=(5, 5), activation='relu'))
-    # model.add(MaxPooling2D(2, 2))
-    # model.add(Conv2D(20, kernel_size=(5, 5), activation='relu'))
-    # model.add(MaxPooling2D(2, 2))
-
-
-    # model.add(Flatten())
-    # model.add(Dense(128, activation='relu'))
     # model.add(Dense(classnum, activation='softmax'))
     # model.compile(loss='categorical_crossentropy',
     #               optimizer='adam', metrics=['accuracy'])
