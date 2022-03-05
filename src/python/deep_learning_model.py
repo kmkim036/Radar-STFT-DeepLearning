787ddd9bc0cc39f0abcfe752ba6e4b6ddfa394b7
kmkim036 <kmkim036@gmail.com>
2022-03-05 22:33:20 +0900

    Fix: set a universal deep learning model

--- src/python/deep_learning_model.py
+++ src/python/deep_learning_model.py
@@ -7,70 +7,15 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 
 
 def create_CNNmodel(lr, img_row, img_col, classnum):
-    # model 1
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu',
-              input_shape=(img_row, img_col, 1)))
-    model.add(MaxPooling2D(2, 2))
-
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(2, 2))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-
-    model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
-    model.add(Dense(classnum, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr), metrics=['accuracy'])
-
-    # model 2
-    model = Sequential()
-    model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1),
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1),
               activation='relu', input_shape=(img_row, img_col, 1)))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-    model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-    model.add(Flatten())
-
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(classnum, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr), metrics=['accuracy'])
-
-    # model 3
-    model = Sequential()
-    model.add(Conv2D(8, kernel_size=(3, 3), activation='relu',
-              input_shape=(img_row, img_col, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Conv2D(16, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dense(classnum, activation='softmax'))
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=Adam(lr), metrics=['accuracy'])
-
-    # model 4
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(5, 5), strides=(1, 1),
-              activation='relu', input_shape=(img_row, img_col, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Conv2D(64, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
-
-    model.add(Conv2D(128, kernel_size=(5, 5),
-              strides=(1, 1), activation='relu'))
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
     model.add(Flatten())
