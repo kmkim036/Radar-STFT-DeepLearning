e40d04ec8516a3ca900d5cedba0b205506062da2
kmkim036 <kmkim036@gmail.com>
2022-03-11 08:25:18 +0900

    Add: one more stage

--- src/python/deep_learning_model_MTL.py
+++ src/python/deep_learning_model_MTL.py
@@ -1,3 +1,4 @@
+from calendar import c
 import tensorflow as tf
 import keras
 from tensorflow.keras.optimizers import Adam
@@ -28,7 +29,16 @@ def create_CNNmodel(classnum_human, classnum_motion, lr, img_row, img_col):
     main_branch2 = Add()([left_branch2, right_branch2])
     main_branch2 = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(main_branch2)
 
-    main_branch = Flatten()(main_branch2)
+    left_branch3 = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding = 'same', activation='relu')(main_branch2)
+    left_branch3 = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(left_branch3)
+
+    right_branch3 = Conv2D(16, kernel_size=(5, 5), strides=(1, 1), padding = 'same', activation='relu')(main_branch2)
+    right_branch3 = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(right_branch3)
+
+    main_branch3 = Add()([left_branch3, right_branch3])
+    main_branch3 = MaxPooling2D(pool_size=(2, 2), strides=(1, 1))(main_branch3)
+
+    main_branch = Flatten()(main_branch3)
     main_branch = Dense(128, activation='relu')(main_branch)
 
     human = Dense(classnum_human, activation='softmax', name = 'human_output')(main_branch)
