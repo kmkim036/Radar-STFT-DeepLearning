e1d0f802bf314035bae4d75a69b8241d58abf116
kmkim036 <kmkim036@gmail.com>
2022-03-18 21:40:38 +0900

    Fix: deep learning model

--- src/python/deep_learning_model.py
+++ src/python/deep_learning_model.py
@@ -10,16 +10,18 @@ def create_CNNmodel(lr, img_row, img_col, classnum):
     model = Sequential()
     model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1),
               activation='relu', input_shape=(img_row, img_col, 1)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
 
     model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
 
     model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
+
+    model.add(Conv2D(16, kernel_size=(3, 3), strides=(1, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(1, 1)))
 
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
     model.add(Dense(classnum, activation='softmax'))
     model.compile(loss='categorical_crossentropy',
                   optimizer=Adam(lr), metrics=['accuracy'])
