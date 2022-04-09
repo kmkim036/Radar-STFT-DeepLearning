db15f2d029aad379b52019444cbd3c4cf30a251a
kmkim036 <kmkim036@gmail.com>
2022-04-09 21:12:39 +0900

    Fix: radar bug && Add: human detect algo

--- src/python/radar_uart_test.py
+++ src/python/radar_uart_test.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.fft import fftshift
-import matplotlib.pyplot as plt
 import math
 
 
@@ -62,12 +61,17 @@ class CircularQueue:
 
 
 # declare circular queue for I-data and Q-data
-I_rawdata_queue = CircularQueue()
-Q_rawdata_queue = CircularQueue()
+I_raw_data_queue = CircularQueue()
+Q_raw_data_queue = CircularQueue()
 
-# declare list fot I-data and Q-data
-I_rawdata = []
-Q_rawdata = []
+# declare list for I-data and Q-data
+I_raw_data = []
+Q_raw_data = []
+
+# fft parameter
+N_FFT_point = 128
+SamplingFreq = 650
+fft_window = signal.windows.hamming(128)
 
 # UART setting and init
 portname = "/dev/ttyACM0"
@@ -77,67 +81,80 @@ radar = serial.Serial(portname, baudrate, timeout=0.3)
 
 def GetRadar():
     # Get radar raw data and save into circular queue
-    rawdata = str(radar.readline(), 'utf-8')
-    if rawdata.find('I') > 0:
+    raw_data = str(radar.readline(), 'utf-8')
+    if raw_data.find('I') > 0:
         for i in range(0, 18):
-            rawdata = str(radar.readline(), 'utf-8')
+            raw_data = str(radar.readline(), 'utf-8')
             if i < 8:
-                rawdata = rawdata.split()
-                list_of_integers = list(map(int, rawdata))
+                if raw_data.find('-') > 0:
+                    # clear queue if async data come in the beginning
+                    I_raw_data_queue.clear()
+                    Q_raw_data_queue.clear()
+                    break
+                raw_data = raw_data.split()
+                list_of_integers = list(map(int, raw_data))
                 for j in list_of_integers:
-                    if I_rawdata_queue.isFull():
-                        I_rawdata_queue.dequeue()
-                    I_rawdata_queue.enqueue(j)
+                    if I_raw_data_queue.isFull():
+                        I_raw_data_queue.dequeue()
+                    I_raw_data_queue.enqueue(j)
             elif i < 10:
                 continue
             else:
-                rawdata = rawdata.split()
-                list_of_integers = list(map(int, rawdata))
+                raw_data = raw_data.split()
+                list_of_integers = list(map(int, raw_data))
                 for j in list_of_integers:
-                    if Q_rawdata_queue.isFull():
-                        Q_rawdata_queue.dequeue()
-                    Q_rawdata_queue.enqueue(j)
-    '''
-    *** need to fix bug under message *** 
-    Traceback (most recent call last):
-        File "src/python/radar_uart_test.py", line 113, in <module>
-            GetRadar()
-        File "src/python/radar_uart_test.py", line 86, in GetRadar
-            list_of_integers = list(map(int, rawdata))
-    ValueError: invalid literal for int() with base 10: '-------------'
-    '''
+                    if Q_raw_data_queue.isFull():
+                        Q_raw_data_queue.dequeue()
+                    Q_raw_data_queue.enqueue(j)
 
 
-# get radar raw data until 1920 samples
-while I_rawdata_queue.isFull() != 1 and Q_rawdata_queue.isFull() != 1:
+def detect_human(image):
+    '''
+    need to fix human detect algorithm
+    '''
+    a = np.argmax(image, axis=0)
+    b = np.argmax(image, axis=1)
+    b = b[6:30]
+    if a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
+        return True
+    else:
+        return False
+
+
+while True:
     GetRadar()
+    # if queue is full, it means that there are enough data for 3 seconds
+    if I_raw_data_queue.isFull() and Q_raw_data_queue.isFull():
+        # load raw data from circular queue into list
+        I_raw_data = I_raw_data_queue.returndata()
+        Q_raw_data = Q_raw_data_queue.returndata()
 
-# load raw data from circular queue into list
-I_rawdata = I_rawdata_queue.returndata()
-Q_rawdata = Q_rawdata_queue.returndata()
+        # make complex input with I-data, Q-data and remove DC component
+        complex_raw_data = []
+        for i in range(0, 1920):
+            complex_raw_data.append(
+                complex(I_raw_data[i], Q_raw_data[i]))
+        complex_raw_data_DC = complex_raw_data - np.mean(complex_raw_data)
 
-# make complex input using I-data and Q-data
-complex_raw_data = []
-for i in range(0, 1920):
-    complex_raw_data.append(
-        complex(I_rawdata[i], Q_rawdata[i]))
-complex_raw_data_DC = complex_raw_data - np.mean(complex_raw_data)
+        # stft
+        f, t, Zxx = signal.stft(x=complex_raw_data_DC, fs=SamplingFreq, window=fft_window,
+                                nperseg=N_FFT_point, nfft=N_FFT_point, noverlap=N_FFT_point//2, boundary=None)
 
-# fft setting and executing
-N_FFT_point = 128
-SamplingFreq = 650
-fft_window = signal.windows.hamming(128)
-f, t, Zxx = signal.stft(x=complex_raw_data_DC, fs=SamplingFreq, window=fft_window,
-                        nperseg=N_FFT_point, nfft=N_FFT_point, noverlap=N_FFT_point//2, boundary=None)
+        # fftshift
+        Zxx = fftshift(abs(Zxx), axes=0)
 
-# make fft result into integer from float
-for i in range(128):
-    for j in range(29):
-        Zxx[i][j] = math.ceil(abs(Zxx[i][j]))
+        # crop image into 36x28 from 128x29
+        Zxx = Zxx[46:82, 1:29]
 
-# fftshift
-Zxx = fftshift(abs(Zxx), axes=0)
+        # make fft result into integer from float
+        for i in range(36):
+            for j in range(28):
+                Zxx[i][j] = math.ceil(abs(Zxx[i][j]))
 
-# plot sample spectrogram for a stft
+        ret = detect_human(Zxx)
+        if ret == True:
+            break
+
 plt.imshow(Zxx, vmin=0, aspect='auto')
+plt.colorbar()
 plt.show()
