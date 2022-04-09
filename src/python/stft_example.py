2ec4c57ed6f0a986406d001fd4d03f19699cfd4a
kmkim036 <kmkim036@gmail.com>
2022-04-09 21:12:03 +0900

    Fix: check stft result to detect human

--- src/python/stft_example.py
+++ src/python/stft_example.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from scipy import signal
 import matplotlib.pyplot as plt
+from scipy.fft import fftshift
+import math
 
-IM_file_name = '220128_1_2_45_IM.txt'
-RE_file_name = '220128_1_2_45_RE.txt'
+IM_file_name = '220128_1_2_30_IM.txt'
+RE_file_name = '220128_1_2_30_RE.txt'
 
 # need to change directory name for own PC
 DirectoryPath = '/home/kmkim/Projects/git/kmkim036/Radar-STFT-DeepLearning/data/220128/'
@@ -31,40 +32,49 @@ while True:
     RE_raw_data.append(int(line))
 f.close()
 
+# make complex input
 complex_raw_data = []
 for i in range(0, 1920):
-    complex_raw_data.append(complex(RE_raw_data[i], IM_raw_data[i]))
+    complex_raw_data.append(
+        complex(RE_raw_data[i], IM_raw_data[i]))
 complex_raw_data_DC = complex_raw_data - np.mean(complex_raw_data)
 
+# fft setting and executing
 N_FFT_point = 128
 SamplingFreq = 650
-fft_window = np.hamming(N_FFT_point)
-
+fft_window = signal.windows.hamming(128)
 f, t, Zxx = signal.stft(x=complex_raw_data_DC, fs=SamplingFreq, window=fft_window,
-                        nperseg=N_FFT_point, nfft=N_FFT_point, noverlap=N_FFT_point/2.2)
+                        nperseg=N_FFT_point, nfft=N_FFT_point, noverlap=N_FFT_point//2, boundary=None)
 
-# print frequency domain length
-print(Zxx.shape[0])
 
-# print time domain length
-print(len(Zxx[0]))
+def crop_image(image):
+    # crop image into 36 x 28 size from 128 x 29
+    crop_image = image[46:82, 1:29]
+    return crop_image
 
-# print abs(stft result) for one frequency range
-print(abs(Zxx[0]))
 
-# plot stft image
-plt.pcolormesh(t, f, abs(Zxx), vmin=0)
-plt.title('STFT Magnitude')
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+# fftshift
+Zxx = fftshift(abs(Zxx), axes=0)
+Zxx = crop_image(Zxx)
+
+# make fft result into integer from float
+for i in range(36):
+    for j in range(28):
+        Zxx[i][j] = math.ceil(abs(Zxx[i][j]))
+
+
+a = np.argmax(Zxx, axis=0)
+print(a)
+b = np.argmax(Zxx, axis=1)
+print(b)
+b = b[6:30]
+
+if a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18:
+    print('check1')
+if b.min() >= 15:
+    print('check2')
+
+# plot sample spectrogram for a stft
+plt.imshow(Zxx, aspect='auto')
+plt.colorbar()
 plt.show()
-
-
-# another library for stft 1
-# import tensorflow as tf
-# results = tf.signal.stft(complex_raw_data_DC, frame_length=N_FFT_point, frame_step=N_FFT_point/2, window_fn=fft_window)
-
-# another library for stft 2
-# import librosa
-# stftdata = librosa.stft(complex_raw_data_DC, n_fft = N_FFT_point, hop_length=N_FFT_point / 2)
-# print(stftdata)
