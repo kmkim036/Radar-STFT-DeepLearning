8883fc0b6c8543b8301ea1ac9c319817689e9873
kmkim036 <kmkim036@gmail.com>
2022-04-10 23:38:40 +0900

    Split file

---
+++ src/python/main/detect.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def detect_human(image):
+    '''
+    need to fix human detect algorithm
+    '''
+    a = np.argmax(image, axis=0)
+    b = np.argmax(image, axis=1)
+    b = b[6:30]
+    if a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
+        return True
+    else:
+        return False
