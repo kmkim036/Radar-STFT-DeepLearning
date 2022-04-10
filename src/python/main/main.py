9c703bd696061dac45a059042516b9010d563b7f
kmkim036 <kmkim036@gmail.com>
2022-04-10 23:38:27 +0900

    Reorganize

--- src/python/main/main.py
+++ src/python/main/main.py
@@ -1,11 +1,9 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
 import radar
 import cirqueue
 import stft
 import spi
 import display
+import detect
 
 
 '''
@@ -22,19 +20,6 @@ while True:
 '''
 
 
-def detect_human(image):
-    '''
-    need to fix human detect algorithm
-    '''
-    a = np.argmax(image, axis=0)
-    b = np.argmax(image, axis=1)
-    b = b[6:30]
-    if a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
-        return True
-    else:
-        return False
-
-
 if __name__ == "__main__":
     # declare circular queue for I-data and Q-data
     I_raw_data_queue = cirqueue.CircularQueue()
@@ -44,24 +29,7 @@ if __name__ == "__main__":
     I_raw_data = []
     Q_raw_data = []
 
-    while True:
-        radar.GetRadar(I_raw_data_queue, Q_raw_data_queue)
-        # if queue is full, it means that there are enough data for 3 seconds
-        if I_raw_data_queue.isFull() and Q_raw_data_queue.isFull():
-            # load raw data from circular queue into list
-            I_raw_data = I_raw_data_queue.returndata()
-            Q_raw_data = Q_raw_data_queue.returndata()
-
-            stft_result = stft.stft_crop(I_raw_data, Q_raw_data)
-
-            ret = detect_human(stft_result)
-            if ret == True:
-                break
-    plt.imshow(stft_result, vmin=0, aspect='auto')
-    plt.colorbar()
-    plt.show()
-
-    '''
+    display.display_init()
     while True:
         radar.GetRadar(I_raw_data_queue, Q_raw_data_queue)
         if I_raw_data_queue.isFull() and Q_raw_data_queue.isFull():
@@ -70,11 +38,12 @@ if __name__ == "__main__":
 
             stft_result = stft.stft_crop(I_raw_data, Q_raw_data)
 
-            ret = detect_human(stft_result)
+            ret = detect.detect_human(stft_result)
+
             if ret == True:
                 spi.send_spi(stft_result)
                 motion, human = spi.receive_spi()
-                display.display_monitor(stft_result, motion, human)
+                # example: 1 = stride, 3 =  Woman 2
+                display.display_result(stft_result, 1, 3)
                 I_raw_data_queue.clear()
                 Q_raw_data_queue.clear()
-    '''
