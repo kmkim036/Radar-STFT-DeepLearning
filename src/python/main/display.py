1881c77054be3da86ae4c6a6bd6de43c2dbbca75
kmkim036 <kmkim036@gmail.com>
2022-04-16 20:09:11 +0900

    Update: text position

--- src/python/main/display.py
+++ src/python/main/display.py
@@ -2,8 +2,8 @@ import pygame
 import matplotlib.pyplot as plt
 
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
 
 SIZE = 100
 
@@ -22,15 +22,16 @@ text_waiting = font.render("waiting...", True, black)
 text_walk = font.render("motion: walk", True, black)
 text_stride = font.render("motion: stride", True, black)
 text_creep = font.render("motion: creep", True, black)
-text_M1 = font.render("human: Man 1", True, black)
-text_M2 = font.render("human: Man 2", True, black)
+text_M1 = font.render("human:  Man 1", True, black)
+text_M2 = font.render("human:  Man 2", True, black)
 text_W1 = font.render("human: Woman 1", True, black)
 text_W2 = font.render("human: Woman 2", True, black)
 
 
 def display_init():
     screen.fill(white)
-    screen.blit(text_waiting, (int(0.42 * SCREEN_WIDTH), int(0.5 * SCREEN_HEIGHT)))
+    screen.blit(text_waiting, (int(0.42 * SCREEN_WIDTH),
+                int(0.5 * SCREEN_HEIGHT)))
     pygame.display.update()
 
 
@@ -42,9 +43,11 @@ def display_result(image, motion, human):
     plt.imshow(image, vmin=0, aspect='auto')
 
     mngr = plt.get_current_fig_manager()
-    window_position = "+" + str(int(0.54 * SCREEN_WIDTH)) + "+" + str(int(0.25 * SCREEN_HEIGHT))
-    mngr.window.wm_geometry(window_position)     # x+y
-    
+    window_position = "+" + \
+        str(int(0.54 * SCREEN_WIDTH)) + "+" + \
+        str(int(0.25 * SCREEN_HEIGHT))    # +x+y
+    mngr.window.wm_geometry(window_position)
+
     plt.show()
     plt.colorbar()
     plt.pause(1)
@@ -65,6 +68,8 @@ def display_result(image, motion, human):
         text_human = text_W1
     else:
         text_human = text_W2
-    screen.blit(text_motion, (int(0.375 * SCREEN_WIDTH), int(0.75 * SCREEN_HEIGHT)))
-    screen.blit(text_human, (int(0.375 * SCREEN_WIDTH), int(0.875 * SCREEN_HEIGHT)))
+    screen.blit(text_motion, (int(0.4 * SCREEN_WIDTH),
+                int(0.75 * SCREEN_HEIGHT)))
+    screen.blit(text_human, (int(0.39 * SCREEN_WIDTH),
+                int(0.875 * SCREEN_HEIGHT)))
     pygame.display.update()
