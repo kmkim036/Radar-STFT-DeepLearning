9fc78cd68f4fbeb5533b3e42306b1ac98a3b13a6
kmkim036 <kmkim036@gmail.com>
2022-04-16 20:10:09 +0900

    Add: power and threshold

--- src/python/main/detect.py
+++ src/python/main/detect.py
@@ -10,8 +10,6 @@ def detect_human(image, power):
     b = b[6:30]
     if power > 5000000:
         if a[0] > a[27] and a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
-            print(a[0])
-            print(a[27])
             return True
         else:
             return False
