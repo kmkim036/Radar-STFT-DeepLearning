a3b4270318efa182b8907f3a8298d87efaa591f8
Kim Kyeong-min <kmkim036@gmail.com>
2022-05-23 14:38:31 +0900

    Add: ksj files

---
+++ src/verilog/Accumulator.v
@@ -0,0 +1,66 @@
+
+module Accumulator(
+	iCLK,
+	iRSTn,
+	iCLR,
+	iEN,
+	iDATA,
+	CNT,
+	oEN,
+	oDATA
+);
+	
+//==========================================================================================//
+//	Parameter
+//==========================================================================================//
+parameter IL = 10;
+parameter END = 9;
+parameter OL = 10;
+//==========================================================================================//
+//	Input Signals
+//==========================================================================================//
+input iRSTn;
+input iCLK;
+input iCLR;
+input iEN;
+input [IL-1 : 0] iDATA;
+input [3:0] CNT;
+//==========================================================================================//
+//	Output Signals
+//==========================================================================================//
+output oEN;
+output oDATA;
+//==========================================================================================//
+//	Internal Signals
+//==========================================================================================//
+wire oEN;
+wire [3:0] CNT;
+wire [OL-1:0]oDATA;
+assign oEN = (CNT == END) ? 1'b1 : 1'b0;
+
+always@(posedge iCLK or negedge iRSTn)
+begin
+
+	if(~iRSTn)
+	begin
+		oDATA <= 0;
+	end
+	
+	else if(iCLR)
+	begin
+		oDATA <= iDATA;
+	end
+
+	else if(iEN)
+	begin
+		oDATA<= oDATA + iDATA;
+	end
+	
+	else
+	begin
+		oDATA <=oDATA;
+	end
+end	
+
+endmodule
+
---
+++ src/verilog/TH_ROM.v
@@ -0,0 +1,23 @@
+`timescale 1 ns / 100 ps
+
+module TH_ROM(TH_addr,oTH);
+
+parameter WN = 10;
+parameter WL = 10;
+
+input [WN-1:0] TH_addr;
+output [WL-1:0] oTH;
+
+reg [WL-1:0] oTH
+
+always@(TH_addr)
+begin
+	case(TH_addr)
+		'd00:begin oTH <= #1 10'b000000000; end
+		
+		default: begin oTH <= 	end
+	endcase
+end
+	
+endmodule
+	
