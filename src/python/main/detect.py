5e6666f51f46770217665e5a24dba44ec52d6a41
kmkim036 <kmkim036@gmail.com>
2022-05-25 13:20:38 +0900

    Fix: detect condition

--- src/python/main/detect.py
+++ src/python/main/detect.py
@@ -9,7 +9,7 @@ def detect_human(image, power):
     b = np.argmax(image, axis=1)
     b = b[6:30]
     if power > 5000000:
-        if a[0] > a[27] and a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
-            return True
+        if a[23] >= 12 and a[23] <= 18 and a[24] >= 12 and a[24] <= 18 and a[25] >= 12 and a[25] <= 18 and a[26] >= 12 and a[26] <= 18 and a[27] >= 12 and a[27] <= 18 and b.min() >= 15:
+           return True
         else:
             return False
