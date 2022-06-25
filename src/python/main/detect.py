0631bcf952748065dd2073b7d17d50db8951acc3
kmkim036 <kmkim036@gmail.com>
2022-06-25 21:45:55 +0900

    Variable: power threshold

--- src/python/main/detect.py
+++ src/python/main/detect.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 time_reference = 23
+power_reference = 10000000
 
 
 def detect_human(image, power):
@@ -10,8 +11,8 @@ def detect_human(image, power):
     a = np.argmax(image, axis=0)
     b = np.argmax(image, axis=1)
     b = b[6:30]
-    
-    if power > 10000000:
+
+    if power > power_reference:
         if a[time_reference] >= 12 and a[time_reference] <= 18 and a[time_reference + 1] >= 12 and a[time_reference + 1] <= 18 and a[time_reference + 2] >= 12 and a[time_reference + 2] <= 18 and a[time_reference + 3] >= 12 and a[time_reference + 3] <= 18 and a[time_reference + 4] >= 12 and a[time_reference + 4] <= 18 and b.min() >= 15:
             return True
         else:
